29454bbb5236f21f19b1e3262e53eeddc9aba5c5
Albert Aleksa <albert.aleksa.by@gmail.com>
2021-12-27 19:53:36 -0600

    SQL. Types of SQL

---
+++ SQL.sql
@@ -0,0 +1,24 @@
+-- SQL
+/*
+DML (Data Manipulation Language):
+ - SELECT
+ - INSERT
+ - UPDATE
+ - DELETE
+ 
+DDL (Data Definition Language):
+ - CREATE
+ - DROP
+ - ALTER
+ - TRUNCATE
+ 
+DCL (Data Control Language):
+ - GRANT
+ - REVOKE
+ 
+TCL (Transaction Control Language):
+ - COMMIT
+ - ROLLBACK
+ - SAVEPOINT
+ - SET TRANSACTION
+*/
