1e90e918d8a5f1c0b2080ef965da331e633f077f
Albert Aleksa <albert.aleksa.by@gmail.com>
2021-12-29 12:38:53 -0600

    give rights to users

---
+++ sys@xepdb1.sql
@@ -0,0 +1,13 @@
+select USER,SYS_CONTEXT ('USERENV', 'SESSION_USER') from dual;
+
+-- give permissions to create view for schema <security>
+grant create view to security;
+
+-- give permissions to create tables for schema <security>
+grant create table to security;
+
+-- give permissions to create view for schema <accountant>
+grant create view to accountant;
+
+-- give permissions to create tables for schema <accountant>
+grant create table to accountant;
