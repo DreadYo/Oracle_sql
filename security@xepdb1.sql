1ae0a65d375bf8a400fcac4e500462d4d560f332
Albert Aleksa <albert.aleksa.by@gmail.com>
2021-12-29 15:24:29 -0600

    views from security schema

--- security@xepdb1.sql
+++ security@xepdb1.sql
@@ -37,4 +37,34 @@ select * from security.duty_v;
 
 select * from security.cars_v;
 
+select USER,SYS_CONTEXT ('USERENV', 'SESSION_USER') from dual;
+
+drop view security.duty_v;
+
+create table duty
+(
+    id  number generated as identity,
+    employee_id     number,
+    duty_date       date
+);
+
+select * from HR.employees;
+
+
+create or replace view duty_employees
+as
+select  emp.first_name,
+        emp.last_name,
+        emp.email,
+        emp.phone_number,
+        emp.hire_date,
+        emp.job_id,
+        emp.salary,
+        emp.commission_pct,
+        dut.duty_date
+from    hr.employees emp
+inner join security.duty dut
+        on emp.employee_id = dut.employee_id   
+;
+
 
