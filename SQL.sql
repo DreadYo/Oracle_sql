d7f0580f711e5b0ecfefc48171e3738ecd9a62b1
Albert Aleksa <albert.aleksa.by@gmail.com>
2021-12-31 01:18:38 -0600

    SQL

--- SQL.sql
+++ SQL.sql
@@ -4,13 +4,13 @@ DML (Data Manipulation Language):
  - SELECT
  - INSERT
  - UPDATE
- - DELETE
+ - DELETE   (the operation is logged in journal. Can be cancelled)
  
 DDL (Data Definition Language):
  - CREATE
  - DROP
  - ALTER
- - TRUNCATE
+ - TRUNCATE (Can't be cancelled)
  - COMMENT
  
 DCL (Data Control Language):
@@ -22,4 +22,10 @@ TCL (Transaction Control Language):
  - ROLLBACK
  - SAVEPOINT
  - SET TRANSACTION
+ 
+ 
+High water mark - how many data was in table.
+Truncate move high water mark point to 0.
+Delete doesn't move. And after delete, query can be work slow, because server will process all blocks when data was before.
+ 
 */
