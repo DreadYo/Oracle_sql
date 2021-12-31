c12c8c11ac1786bb6907ec0211abecbb4037c2eb
Albert Aleksa <albert.aleksa.by@gmail.com>
2021-12-30 20:30:39 -0600

    Queries for SYS schema

--- sys@xepdb1.sql
+++ sys@xepdb1.sql
@@ -12,8 +12,12 @@ grant create view to accountant;
 -- give permissions to create tables for schema <accountant>
 grant create table to accountant;
 
+-- give permissions to reference to table <hr.employees> for schema <accountant>
+GRANT REFERENCES ON hr.employees TO accountant;
 
 grant create sequence to security;
+
+grant create sequence to accountant;
 
 grant select on hr.employees to security with grant option;
 
@@ -21,3 +25,5 @@ grant select on hr.employees to security with grant option;
 grant select on seller.maker to security with grant option;
 
 revoke select on seller.maker from security;
+
+grant select on hr.employees to accountant with grant option;
